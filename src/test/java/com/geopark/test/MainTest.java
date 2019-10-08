14d5e2f20c2ddac83bc8a99ff57ac8ae3ca4b863
iqeq00 <iqeq00@gmail.com>
2019-10-08 22:31:25 +0800

    Modify MainTest

--- src/test/java/com/geopark/test/MainTest.java
+++ src/test/java/com/geopark/test/MainTest.java
@@ -2,12 +2,20 @@ package com.geopark.test;
 
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainTest {
 
     public static void main(String[] args) {
 
-        System.out.println(cleanContextPath("/etc"));
-        System.out.println("ending");
+//        System.out.println(cleanContextPath("/etc"));
+//        System.out.println("ending");
+
+        Set<String> orderIdList = new HashSet<>(
+                Arrays.asList(StringUtils.split(null, ",")));
+
     }
 
     private static String cleanContextPath(String contextPath) {
